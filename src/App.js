c0ae4b22fa6fce3a72eedc9351e22bb705ae9731
Jerard Guevarra <jerardlguevarra@gmail.com>
2024-02-13 14:46:30 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [text, setText] = useState('Will you be my valentine?');
+  const [img, setImg] = useState("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWphMHVqczg1NHFocDN5bGk3a2Mzd2t4cnpzdGtzcjBjbDVhcjdmeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/SoZAMdHWldHbQHLCGZ/giphy.gif");
+  const [buttonSize, setButtonSize] = useState(16);
+  const sure = ["Are you sure?", "Are you really sure?", "Are you really REALLY sure???", "Are you SURE you're sure that you're sure???"];
+  function yesClick() {
+    setText('HOORAY <3');
+    setImg("https://i.pinimg.com/originals/14/12/a6/1412a602c0cd0027a7c494967a564e7e.gif");
+  }
+
+  function noClick() {
+    var num = Math.floor(Math.random() * 4);
+    setText(sure[num]);
+    setButtonSize(buttonSize + 36);
+    console.log(buttonSize);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={img} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="yes" style={{fontSize:buttonSize}} onClick={yesClick}>
+            Yes
+          </button>
+          <button className="no" style={{fontSize:16}} onClick={noClick}>
+            No
+          </button>
+      
       </header>
     </div>
   );
 }
-
+/*
+Note:
+Page is literally just like gif image of hello kitty then
+Will you be my valentine
+Yes
+No
+If yes redirect to OKAY YAY
+If no make the yes button bigger
+*/
 export default App;
